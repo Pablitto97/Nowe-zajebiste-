0fa530bb46f6ecb55e376858cc6d1fb7bc504992
Paweu <pawel99730@interia.pl>
2026-04-26 14:14:04 +0200

    clean initial commit

---
+++ Projekt 2/pierwszy.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+
+int main() {
+	std::string imie;
+	int wiek, wynik;
+	std::cout << "Wpisz swoje imie\n";
+	std::cin >> imie;
+	std::cout << "Wpisz ile masz lat\n";
+	std::cin >> wiek;
+	std::cout << "witaj " << imie << "! masz " << wiek << " lat\n";
+	wynik = 75 - wiek;
+	if (wynik < 0) {
+		std::cout << "Gratulacje! wd statystyk nie powinienes juz zyc od " << -wynik << " lat\n";
+	}
+	else if (wynik > 0) {
+		std::cout << "Biorac srednia dlugosc zycia zostalo Ci jeszcze " << wynik << " lat zycia\n";
+	}
+	else {
+		std::cout << "Wedlug statystyk umrzesz w tym roku";
+	}
+	std::cin.ignore();
+	std::cin.get();
+	return 0;
+}
