96a82abeafb7a4a862607a0be70fdc14bcbb7a5e
Paweu <pawel99730@interia.pl>
2026-04-27 21:53:37 +0200

    letkie poprawki

--- Projekt 2/pierwszy.cpp
+++ Projekt 2/pierwszy.cpp
@@ -3,15 +3,33 @@
 
 int main() {
 	std::string imie;
-	int wiek, wynik;
+	int wiek, wynik ,srednia;
+	char plec;
 	std::cout << "Wpisz swoje imie\n";
 	std::cin >> imie;
 	std::cout << "Wpisz ile masz lat\n";
 	std::cin >> wiek;
 	std::cout << "witaj " << imie << "! masz " << wiek << " lat\n";
-	wynik = 75 - wiek;
+	while (true) {
+		std::cout << "Wpisz swoja plec k/m\n";
+		std::cin >> plec;
+		if (plec == 'k') {
+			srednia = 87;
+			std::cout << "srednia dlugosc zycia kobiet to 87lat\n";
+			break;
+		}
+		else if (plec == 'm') {
+			srednia = 75;
+			std::cout << "srednia dlugosc zycia meszczyzn to 75lat\n";
+			break;
+		}
+		else {
+			std::cout << "niepoprawna odpowiedz\n";
+		}
+	}
+	wynik = srednia - wiek;
 	if (wynik < 0) {
-		std::cout << "Gratulacje! według statystyk nie powinienes juz zyc od " << -wynik << " lat\n";
+		std::cout << "Gratulacje! wedlug statystyk nie powinienes juz zyc od " << -wynik << " lat\n";
 	}
 	else if (wynik > 0) {
 		std::cout << "Biorac srednia dlugosc zycia zostalo Ci jeszcze " << wynik << " lat zycia\n";
